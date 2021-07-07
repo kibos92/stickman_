171fa89e9bcfc30940b72b304255169b5a3004cf
sobik.sebastian@gmail.com <sobik.sebastian@gmail.com>
2021-07-07 20:43:01 +0200

    ?

--- Form1.cs
+++ Form1.cs
@@ -17,34 +17,36 @@ namespace stickman_
         {
             InitializeComponent();
         }
-        static int time = 1;
+        private int time = 0;
         private void Projectile()
         {
+            while (time <= 10)
+            { 
                 int speed = 1;
                 int angle = 30;
 
                 double[] position = { arrow.Location.X, arrow.Location.Y };
-
                 double xvelocity = speed * Math.Cos(angle);
                 double yvelocity = speed * Math.Sin(angle);
 
                 position[0] *= (xvelocity * time);
                 position[1] *= (yvelocity * time);
 
-                arrow.Location = new Point(Convert.ToInt32(position[0]), Convert.ToInt32(position[1]));
+                arrow.Location= new Point(Convert.ToInt32(position[0]),Convert.ToInt32(position[1]));
 
                 time++;
-            
+            }
         }
+
 
         private void clock_Tick(object sender, EventArgs e)
         {
-            
+            Projectile();
         }
 
         private void stickman_Click(object sender, EventArgs e)
         {
-            Projectile();
+            
         }
     }
 }
